394f2ca9789e26b4887fc609787dde5b4d98e737
narender046 <40541201+narender046@users.noreply.github.com>
2018-08-11 23:02:04 +0530

    jenkisn file added

---
+++ JenkinsFile.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agents any{
+ stages{
+     stage('init'){
+     
+     steps {
+
+         "echo testing....."
+         }
+      stage('Build'){
+     
+     steps {
+
+         "echo testing....."
+         }
+         stage('Deploy'){
+     
+     steps {
+
+         "echo testing....."
+         } 
+     }
+ }
+    }
+
+}
